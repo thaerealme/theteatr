1841e3ec7b148cf1ec32aecc054c74b4805ff8f3
Artem Khusnullin <twentyfortythree@yandex.ru>
2023-02-16 14:38:17 +0500

    Первая версия, шапка и мобильное меню

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger_opened');
+  mobileMenu.classList.toggle('mobile-menu_opened')
+});
